589468268d31add79c97e0615b0beabc75095c8a
Meital Atia <meitalatia@outlook.co.il>
2018-09-03 12:31:05 +0300

    add user class - day 1 - 12:30

---
+++ EventsUs/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsUs.Models
+{
+    public class User:IdentityUser
+    {
+        public string name { get; set; }
+
+        public string pass { get; set; }
+
+        public int ID { get; set; }
+
+        public string email { get; set; }
+
+        public int age { get; set; }
+
+        List<User> list_friednds = new List<User>();
+        List<Event> list_events = new List<Event>();
+
+    }
+}
