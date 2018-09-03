7432a556d8eff3f488598441ef000155d01b5390
Meital Atia <meitalatia@outlook.co.il>
2018-09-03 13:01:44 +0300

    user change

--- EventsUs/Models/User.cs
+++ EventsUs/Models/User.cs
@@ -8,13 +8,6 @@ namespace EventsUs.Models
 {
     public class User:IdentityUser
     {
-        public string name { get; set; }
-
-        public string pass { get; set; }
-
-        public int ID { get; set; }
-
-        public string email { get; set; }
 
         public int age { get; set; }
 
