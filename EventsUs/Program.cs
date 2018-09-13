5d5b87e35b15841de28177eafe5e655638157141
Alex <alex11535@bezeqint.net>
2018-09-13 16:11:55 +0300

    fixeddddd

--- EventsUs/Program.cs
+++ EventsUs/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace EventsUs
-{
+{ 
     public class Program
     {
         public static void Main(string[] args)
