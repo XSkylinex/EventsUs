f71c3dcb3f74830accca554f39873247b858f54b
dvir <dvir16@gmail.com>
2018-09-16 13:33:14 +0300

    weather web servise

---
+++ EventsUs/Models/GlobalEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsUs.Models
+{
+    public class GlobalEvent
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public string Location { get; set; }
+    }
+}
