7b06449b0623c8913482bebaccfe3298d126e84f
Meital Atia <meitalatia@outlook.co.il>
2018-09-16 15:18:25 +0300

    update profile

--- EventsUs/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EventsUs/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -44,6 +44,9 @@ namespace EventsUs.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            [Display(Name = "Full Name")]
+            public string Name { get; set; }
+
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
