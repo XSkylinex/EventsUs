0c1a371a49154430cb5b649d8f089f6cf1ffd436
Meital Atia <meitalatia@outlook.co.il>
2018-09-16 18:25:02 +0300

    findfriends

--- EventsUs/Controllers/HomeController.cs
+++ EventsUs/Controllers/HomeController.cs
@@ -5,11 +5,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EventsUs.Models;
+using EventsUs.Data;
 
 namespace EventsUs.Controllers
 {
+    
     public class HomeController : Controller
     {
+
+
         public IActionResult Index()
         {
             return View();
