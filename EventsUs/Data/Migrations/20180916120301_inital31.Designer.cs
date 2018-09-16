b16060496f7a03dffe716b195034cd216f02d3aa
dvir <dvir16@gmail.com>
2018-09-16 15:26:33 +0300

    youtube add

--- EventsUs/Data/Migrations/20180916120301_inital31.Designer.cs
+++ EventsUs/Data/Migrations/20180916120301_inital31.Designer.cs
@@ -4,14 +4,16 @@ using EventsUs.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EventsUs.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180916120301_inital31")]
+    partial class inital31
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EventsUs/Data/Migrations/20180916120301_inital31.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventsUs.Data.Migrations
+{
+    public partial class inital31 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
