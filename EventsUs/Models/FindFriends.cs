4741484a74dd0f0961ef1fea12d53dde2cc644da
Meital Atia <meitalatia@outlook.co.il>
2018-09-16 17:29:38 +0300

    adding findfriends - not finish yet

---
+++ EventsUs/Models/FindFriends.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsUs.Models
+{
+    public class FindFriends
+    {
+        public string ID { get; set; }
+    }
+}
